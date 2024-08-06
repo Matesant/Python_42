0a97b80218888fbf72031e494585174bcf1bf133
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-08-06 16:55:55 -0300

    Cell03 & cell04

---
+++ discovery_piscine/cell03/ex02/i_got_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+	input("What you gotta say?: ")
+	while True:
+		answer = input("I got that! Anything else?: ")
+		if answer == "STOP":
+			break
+	
+if __name__ == "__main__":
+	main()	
