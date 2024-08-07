438044e36e1196f07e4a4408147de85bdd114e31
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-08-07 16:41:47 -0300

    Cell05

---
+++ discovery_piscine/cell05/ex14/free_range.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from sys import argv
+from re import findall
+
+def main():
+	try:
+		if (len(argv) != 3):
+			print("none")
+			return
+		number1 = int(argv[1])
+		number2 = int(argv[2])
+		if number1 < number2:
+			array = list(range(number1, number2 + 1))
+		else:
+			array = list(range(number2, number1 + 1))
+		print(array)
+	except:
+		print("none")
+		return
+
+	
+	
+
+if __name__ == "__main__":
+	main()
