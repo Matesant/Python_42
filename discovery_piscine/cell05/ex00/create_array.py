b1d333a8179e126cd194780133f28fe8c292e45c
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-08-07 11:12:48 -0300

    Cell05 ex00

---
+++ discovery_piscine/cell05/ex00/create_array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def main():
+	array = list()
+	array.append(2)
+	array.append(8)
+	array.append(9)
+	array.append(48)
+	array.append(8)
+	array.append(22)
+	array.append(-12)
+	array.append(2)
+	print(array)
+
+if __name__ == "__main__":
+	main()
