acdd24cada3ca0ced17f22a32fcc599d887203a7
Mateus Santos do nascimento <134240033+Matesant@users.noreply.github.com>
2024-08-08 08:22:29 -0300

    Update play_with_arrays.py

--- discovery_piscine/cell05/ex01/play_with_arrays.py
+++ discovery_piscine/cell05/ex01/play_with_arrays.py
@@ -3,7 +3,7 @@ def main():
 	array = [2, 8, 9, 48, 8, 22, -12, 2]
 	array2 = [i + 2 for i in array]
 	print(f"Original array: {array}")
-	print(f"New array: {array2})
+	print(f"New array: {array2}")
 
 if __name__ == "__main__":
 	main()
