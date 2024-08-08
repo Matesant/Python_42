d912ad186468955667e62d82304e7c113e33bed7
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-08-08 16:36:03 -0300

    Cell07

--- discovery_piscine/cell07/your_namebook.py
+++ discovery_piscine/cell07/your_namebook.py
@@ -2,15 +2,15 @@
 from sys import argv
 
 def array_of_names(persons: dict) -> list:
-	array = list(persons.keys())
+    return [f"{key.capitalize()} {value.capitalize()}" for key, value in persons.items()]
 
 def main():
 	persons = {
-"jean": "valjean",
-"grace": "hopper",
-"xavier": "niel",
-"fifi": "brindacier"
-}
+        "jean": "valjean",
+        "grace": "hopper",
+        "xavier": "niel",
+        "fifi": "brindacier"
+    }
 	print(array_of_names(persons))
 
 if __name__ == "__main__":
